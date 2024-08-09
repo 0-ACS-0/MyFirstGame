e57c34c8c948474935d40a702fe08dc00c544079
0-ACS-0 <antonio.carreteros@outlook.com>
2024-08-09 14:16:14 +0200

    + Big changes

---
+++ Idk/Game/inc/conGraph.h
@@ -0,0 +1,19 @@
+#ifndef CONGRAFH_H
+#define CONGRAFH_H
+
+/* Type definitions | Definiciones de tipos */
+typedef unsigned short uint16_t;
+
+/* Console graphics  */
+typedef struct {
+    
+}congraph_h;
+
+/* External functions prototypes | Prototipos de las funciones externas */
+extern void print(char *);
+extern void scan(char *, uint16_t);
+
+/* Functions prototypes | Prototipos de las funciones */
+
+
+#endif
